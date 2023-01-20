70f3d5e9fb0d43ecf4fea9736fc373c977b38468
りう <123007508+riunanon@users.noreply.github.com>
2023-01-20 15:44:49 +0900

    def add

--- twitter.py
+++ twitter.py
@@ -1,1 +1,24 @@
 import requests
+def email_check(email):
+    url='https://api.twitter.com/i/users/email_available.json?email='+email
+    response=requests.get(url).json()
+    if response['valid']:
+        return True
+    else:
+        return False
+
+def username_check(username):
+    url='https://api.twitter.com/i/users/username_available.json?username='+username
+    response=requests.get(url).json()
+    if response['valid']:
+        return True
+    else:
+        return False
+
+def phonenumber_check(phonenumber):
+    url='https://api.twitter.com/i/users/phonenumber_available.json?phonenumber='+phonenumber
+    response=requests.get(url).json()
+    if response['valid']:
+        return True
+    else:
+        return False
