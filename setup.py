1784d0af951a60d89c8c4853ce968d34dfe0baaf
りう <123007508+riunanon@users.noreply.github.com>
2023-01-20 16:03:06 +0900

    delete

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='twitter_checker',
-    version='1.0',
-    description='test',
-    author='riu',
-    url='https://github.com/riunanon/twittter_checker',
-    packages=['twitter'],
-    )
