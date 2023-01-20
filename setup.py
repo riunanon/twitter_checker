ef48747c53c736ad4f20d6e8c61f0048a8632470
りう <123007508+riunanon@users.noreply.github.com>
2023-01-20 15:57:19 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='twitter_checker',
+    version='1.0',
+    description='test',
+    author='riu',
+    url='https://github.com/riunanon/twittter_checker',
+    packages=['twitter'],
+    )
